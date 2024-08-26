8414dd6401bd22405e5bac540ed245c1890f0b36
Usman Rashid <usman@smme.edu.pk>
2024-08-26 17:19:34 +1200

    Added assertion for svg

--- nf_core/components/create.py
+++ nf_core/components/create.py
@@ -777,7 +777,7 @@ class ComponentCreate(ComponentCommand):
         if is_stub:
             return power_assertions
 
-        non_stable_outputs = ["bam", "cram", "txt", "log", "gz", "rds", "png", "vcf", "tbi", "zip"]
+        non_stable_outputs = ["bam", "cram", "txt", "log", "gz", "rds", "png", "svg", "vcf", "tbi", "zip"]
 
         outputs_str = " ".join([f"{key} {value}" for (key, value) in component_outputs.items()]).lower()
         has_non_stable = any([ns_output in outputs_str for ns_output in non_stable_outputs])
@@ -819,6 +819,10 @@ class ComponentCreate(ComponentCommand):
                 continue
 
             if "txt" in output_name or "txt" in output_meta:
+                power_assertions += f"\n\t\t\t\t\tfile(process.out.{output_name}[0][1]).readLines()[3..7],"
+                continue
+
+            if "svg" in output_name or "svg" in output_meta:
                 power_assertions += f"\n\t\t\t\t\tfile(process.out.{output_name}[0][1]).readLines()[3..7],"
                 continue
 
